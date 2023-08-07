0278f1b22ae79d2c3720f36c6623bf111864fd42
Minhajulislam0 <mdmishu98730@gmail.com>
2023-08-08 03:58:25 +0600

    Critic Score Added

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,22 @@
+import { Badge } from "@chakra-ui/react";
+import React from "react";
+
+interface Props {
+  score: number;
+}
+
+function CriticScore({ score }: Props) {
+  const color = score > 80 ? "green" : score > 65 ? "yellow" : "";
+  return (
+    <Badge
+      colorScheme={color}
+      fontSize={"14px"}
+      paddingX={2}
+      borderRadius={"4px"}
+    >
+      {score}
+    </Badge>
+  );
+}
+
+export default CriticScore;
