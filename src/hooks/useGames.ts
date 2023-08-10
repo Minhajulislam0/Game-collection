f9f23c6e7f67e39aa70488030665cd0fe0ff9679
Minhajulislam0 <mdmishu98730@gmail.com>
2023-08-10 20:03:36 +0600

    Loading skeleton added

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,15 +24,21 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const controller = new AbortController();
 
+    setLoading(true);
     apiClint
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort();
@@ -41,6 +47,7 @@ const useGames = () => {
   return {
     games,
     error,
+    loading,
   };
 };
 
