2c3684b87303d95e8d669594ad4c29518f19a973
Minhajulislam0 <mdmishu98730@gmail.com>
2023-08-11 00:45:19 +0600

    Generic data fetching hook created

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -4,7 +4,7 @@ import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 
 function GameGrid() {
-  const { games, error, loading } = useGames();
+  const { data, error, loading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
@@ -16,7 +16,7 @@ function GameGrid() {
       >
         {loading &&
           skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
       </SimpleGrid>
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,45 +1,10 @@
-import { useEffect, useState } from "react";
-import apiClint from "../services/api-clint";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
-interface Genre {
+export interface Genre {
   id: number;
   name: string;
 }
 
-interface FetchgenresResponse {
-  count: number;
-  results: Genre[];
-}
-
-const useGenres = () => {
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [error, setError] = useState("");
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-    apiClint
-      .get<FetchgenresResponse>("/genres", { signal: controller.signal })
-      .then((res) => {
-        setGenres(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-
-    return () => controller.abort();
-  }, []);
-
-  return {
-    genres,
-    error,
-    loading,
-  };
-};
+const useGenres = () => useData<Genre>("/genres");
 
 export default useGenres;
