f1c886a42c00da31df3d6837fa5b073da984568e
Minhajulislam0 <mdmishu98730@gmail.com>
2023-08-18 23:56:03 +0600

    Search input added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.png";
 import ColorToggleSwitch from "./ColorToggleSwitch";
+import SearchInput from "./SearchInput";
 
 function Navbar() {
   return (
     <HStack justifyContent="space-between" padding="10px">
       <Image src={logo} boxSize="60px" />
+      <SearchInput />
       <ColorToggleSwitch />
     </HStack>
   );
---
+++ src/components/SearchInput.tsx
@@ -0,0 +1,9 @@
+import { Input } from "@chakra-ui/react";
+
+function SearchInput() {
+  return (
+    <Input borderRadius={20} placeholder="Search Games..." variant={"filled"} />
+  );
+}
+
+export default SearchInput;
