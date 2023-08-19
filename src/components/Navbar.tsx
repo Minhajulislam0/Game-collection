0d600497bf0c603b8c2d097677a6311f5e36f8f2
Minhajulislam0 <mdmishu98730@gmail.com>
2023-08-19 23:48:28 +0600

    Search Implimented

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,15 @@ import logo from "../assets/logo.png";
 import ColorToggleSwitch from "./ColorToggleSwitch";
 import SearchInput from "./SearchInput";
 
-function Navbar() {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+function Navbar({ onSearch }: Props) {
   return (
     <HStack padding="10px">
       <Image src={logo} boxSize="60px" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorToggleSwitch />
     </HStack>
   );
